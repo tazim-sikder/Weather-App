3117942fe324a507e6a6d85a761cb4def098ed9c
Tazim Sikder <sikdertm@miamioh.edu>
2021-05-27 23:19:42 -0400

    change

--- script.js
+++ script.js
@@ -2,61 +2,60 @@ const tabs = document.querySelectorAll('[data-tab-target]');
 const tabContent = document.querySelectorAll('[data-tab-content]')
 
 tabs.forEach(tab => {
-    tab.addEventListener('click', () => {
-      const target = document.querySelector(tab.dataset.tabTarget)
-      tabContent.forEach(tabContent => {
-        tabContent.classList.remove('active')
-      })
-      tabs.forEach(tab => {
-        tab.classList.remove('active')
-      })
-      tab.classList.add('active')
-      target.classList.add('active')
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.tabTarget)
+    tabContent.forEach(tabContent => {
+      tabContent.classList.remove('active')
     })
+    tabs.forEach(tab => {
+      tab.classList.remove('active')
+    })
+    tab.classList.add('active')
+    target.classList.add('active')
   })
+})
 
 let weather = {
-    apiKey : "81de9cf9dbb574d3fdd8afb6fcc40a73",
-    fetchWeather: function (city) {
-      fetch(
-        "https://api.openweathermap.org/data/2.5/weather?q=" +
-          city + "&units=metric&appid=" + this.apiKey
-      )
-        .then((response) => {
-          if (!response.ok) {
-            alert("Enter a valid city.");
-          }
-          return response.json();
-        })
-        .then((data) => this.displayWeather(data));
-    },
-    displayWeather: function (data) {
-      const { name } = data;
-      const { icon, description } = data.weather[0];
-      const { temp, humidity } = data.main;
-      const { speed } = data.wind;
-      document.querySelector(".city").innerText = "Weather in " + name;
-      document.querySelector(".icon").src =
-        "https://openweathermap.org/img/wn/" + icon + ".png";
-      document.querySelector(".description").innerText = description;
-      document.querySelector(".temp").innerText = temp + "°C";
-      document.querySelector(".humidity").innerText =
-        "Humidity: " + humidity + "%";
-      document.querySelector(".wind").innerText =
-        "Wind Speed: " + speed + " km/h";
-    },
-    search: function () {
-      this.fetchWeather(document.querySelector("#search-bar").value);
-    },
-  };
-  
-  document.querySelector("#search-btn").addEventListener("click", function () {
+  apiKey: "81de9cf9dbb574d3fdd8afb6fcc40a73",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city + "&units=metric&appid=" + this.apiKey
+    )
+      .then((response) => {
+        if (!response.ok) {
+          alert("Enter a valid city.");
+        }
+        return response.json();
+      })
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText =
+      "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText =
+      "Wind Speed: " + speed + " km/h";
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector("#search-bar").value);
+  },
+};
+
+document.querySelector("#search-btn").addEventListener("click", function () {
+  weather.search();
+});
+
+document.querySelector("#search-bar").addEventListener("keyup", function (event) {
+  if (event.key == "Enter") {
     weather.search();
-  });
-  
-  document.querySelector("#search-bar").addEventListener("keyup", function (event) {
-      if (event.key == "Enter") {
-        weather.search();
-      }
-    });
-  
+  }
+});
